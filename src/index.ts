e616f6ce2a9574a7e576f2642d571f69f1c950b1
Heng Yin <maple1058@hotmail.com>
2024-12-25 19:37:25 +0800

    export type

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { Create as CreateAvalon, UpdateRecentTeamMember, UpdateRecentTeamVote, UpdateResentQuestVote, SetNextLadyOfTheLake } from "./avalon"
+export type { TAvalon } from "./avalon"
+export type { TQuest, TTeam } from "./quest"
+export type { TRule } from "./rule"
