a17f47f8bf5f6ce091cb08bd42064c38df5d256a
Yin Heng <maple1058@hotmail.com>
2024-12-26 23:37:40 +0800

    change imp

--- src/rule.ts
+++ src/rule.ts
@@ -1,5 +1,5 @@
-import { TCharacterKey } from "./character"
-import { randomArray } from "./tools";
+import { TCharacterKey } from './character';
+import { randomArray } from './tools';
 
 type TVisibilityRule = {
     title: string;
@@ -279,5 +279,5 @@ const innerRules: TInnerRule[] = [{
  * @returns The number of players that are supported.
  */
 export const SupportNumberOfPlayer = (hasLancelot: boolean = false) => {
-    return innerRules.filter(i => i.hasLancelot === hasLancelot).map(i => i.numberOfPlayer)
+    return innerRules.filter(i => i.hasLancelot && hasLancelot).map(i => i.numberOfPlayer)
 }
